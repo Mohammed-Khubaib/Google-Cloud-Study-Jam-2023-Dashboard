a309f01a3fb6b2458b7f02224acda4cbf3432234
Khubaib <mohmmedkhubaib2001@gmail.com>
2023-10-27 17:53:32 +0530

    replaced st.experimental_connection with st.connection

--- SheetsConnection.py
+++ SheetsConnection.py
@@ -3,6 +3,6 @@ import streamlit as st
 def File():
     url = "https://docs.google.com/spreadsheets/d/1OQEZzqf0tl5xrRAg0QQkBwI3ylXNlrAqIG5cMmD8VyE/edit#gid=0"
     # url = "https://docs.google.com/spreadsheets/d/1OQEZzqf0tl5xrRAg0QQkBwI3ylXNlrAqIG5cMmD8VyE/edit?usp=sharing"
-    conn = st.experimental_connection("gsheets", type=GSheetsConnection)
+    conn = st.connection("gsheets", type=GSheetsConnection)
     file = conn.read(spreadsheet=url, usecols=[0, 1,2,3,4,5,6,7,8,9,10])
     return file
